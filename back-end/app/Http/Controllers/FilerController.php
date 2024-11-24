63305299f196c5984d805bb1c803cd5908281702
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-11-25 01:17:58 +0300

    Update testing with adding file paths

--- back-end/app/Http/Controllers/FilerController.php
+++ back-end/app/Http/Controllers/FilerController.php
@@ -9,9 +9,17 @@ class FilerController extends Controller{
     public function __invoke(Request $request){
         
         $file = $request->file('file');
-        return Storage::makeDirectory('file-storage'); //creates a folder
-        //Storage::putFile('file-storage', $file);
+        //return Storage::makeDirectory('file-storage'); //creates a folder
+        Storage::putFile('file-storage', $file);
 
         //test
     }
+
+    public function uploadFile(Request $request){
+        $file = new File;
+        $file->workspace_id = $request->workspace_id;
+        $file->name = $request->name;
+        //$file->path = ;
+    }
+    
 }
