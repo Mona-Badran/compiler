8e045dbc3eb45c0f993bf7c6df6c84290b2bdf50
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-25 01:28:04 +0200

    mmigrations updated

--- back-end/database/migrations/2024_11_24_202408_create_invitations_table.php
+++ back-end/database/migrations/2024_11_24_202408_create_invitations_table.php
@@ -4,9 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateInvitationsTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('invitations', function (Blueprint $table) {
             $table->id();
@@ -18,8 +21,11 @@ class CreateInvitationsTable extends Migration
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('invitations');
     }
-}
+};
