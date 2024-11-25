8d664daf2dc7c29ccc667011961013a9a9ef9fe5
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-11-25 22:21:05 +0300

    Adding methods that need to be implemented

--- back-end/resources/views/post-mail.blade.php
+++ back-end/resources/views/post-mail.blade.php
@@ -1,4 +1,4 @@
 <div>
     <h1>You ar invited to participate in file: {{$data['file']}}</h1>
-    <p>Congrats on being invited to the workspace/file</p>
+    <p>Congrats on being invited to the workspace/file!!!</p>
 </div>
