72d18de530cede789c1dd5a88502029e259509a9
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-25 10:13:26 +0200

    BackEnd workspace-model updated

--- back-end/app/Models/Workspace.php
+++ back-end/app/Models/Workspace.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Workspace extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function files()
+    {
+        return $this->hasMany(File::class);
+    }
 }
