38f203e31f68b338b83aef8b5e769e7c3983ace3
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-11-26 03:17:24 +0300

    Added restriction for users not to invite themselves to workspace

--- back-end/app/Http/Controllers/InvitationController.php
+++ back-end/app/Http/Controllers/InvitationController.php
@@ -7,13 +7,36 @@ use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Mail;
 
-
-use App\Mail\SendingMail;
+use App\Models\User;
 use App\Models\Post;
+use App\Mail\SendingMail;
+use App\Models\Workspace;
 use App\Models\Invitation;
+use App\Models\Collaboration;
 
 class InvitationController extends Controller{
     public function sendEmail(Request $request){
+        
+        //i want to check if the reciever email
+        //created the workspaces that he is inviting others to
+
+        $recipient_user = User::where("email", $request->recipient_email)->first();
+        if(!$recipient_user){
+            return response()->json([
+                "error" => "email not found"
+            ],404);
+        }
+
+        $workspaceInvited = Workspace::where("users_id", $recipient_user->id)
+                                        ->where("id", $request->workspaces_id)
+                                        ->first();
+
+        if($workspaceInvited){
+            return response()->json([
+                "error" => "You cant invite youself to your workspace"
+            ],400);
+        }
+
         $invitation = new Invitation;
         $invitation->sender_id = $request->sender_id;
         $invitation->recipient_email = $request->recipient_email;
@@ -24,12 +47,15 @@ class InvitationController extends Controller{
         Mail::to($invitation->recipient_email)->send(new SendingMail([
             "workspace" => $request->workspaces_id
         ]));
-        $recipient_user = User::where("email", $request->recipient_email)->first();
+        //$recipient_user = User::where("email", $request->recipient_email)->first();
         
+
         $collaboration = new Collaboration;
         $collaboration->users_id = $recipient_user->id;
         $collaboration->workspaces_id = $request->workspaces_id;
         $collaboration->invitations_id = $invitation->id;
+        $collaboration->save();
+
         
     }
 
--- back-end/app/Models/Invitation.php
+++ back-end/app/Models/Invitation.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Invitation extends Model{
     protected $fillable = ["sender_id", "recipient_email", "file_id",
 "status"];
